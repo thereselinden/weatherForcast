e9067a3126b236aa3a3c7eddf8f7af2e1d0e6f60
Therese Linden <theresemlinden@gmail.com>
2023-02-14 13:03:27 +0100

    fetch current weather for stockholm, functions to calculate local time and show temperature with rounded numbers, basic html to print current weather to dom

---
+++ src/script.ts
@@ -0,0 +1,77 @@
+/* const lat: number = 51.509865;
+const long: number = -0.118092; */
+
+const lat: number = 59.334591;
+const long: number = 18.06324;
+
+interface CurrentWeather {
+  city: string;
+  localTime: string;
+  temperature: number;
+  weather: string;
+  // mfl
+}
+
+const getLocalTime = (time: number, timezone: number): string => {
+  const localTime = new Date((time + timezone) * 1000);
+  const getHours = localTime.getUTCHours();
+  const getMinutes = localTime.getUTCMinutes();
+
+  return `${getHours}:${getMinutes}`;
+};
+
+const getTemperature = (temp: number): number => Math.round(temp);
+
+const printCurrentWeather = (weather: CurrentWeather) => {
+  const city = document.querySelector('#city') as HTMLHeadingElement;
+  const localTime = document.querySelector('#localTime') as HTMLHeadingElement;
+  const temperature = document.querySelector(
+    '#temperature'
+  ) as HTMLParagraphElement;
+  const weatherDescription = document.querySelector(
+    '#weatherDescription'
+  ) as HTMLParagraphElement;
+
+  city.textContent = weather.city;
+  localTime.textContent = weather.localTime;
+  temperature.innerHTML = `${weather.temperature.toString()} &#8451`;
+  weatherDescription.textContent = weather.weather;
+
+  console.log(weather);
+};
+
+// fetch för current
+const fetchCurrentWeather = async (
+  lat: number,
+  long: number
+): Promise<void> => {
+  //https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=a0a9672a941bc58ae811a05987143dd5`
+    );
+    const data = await response.json();
+
+    console.log(data);
+
+    const currentWeather: CurrentWeather = {
+      city: data.name,
+      localTime: getLocalTime(data.dt, data.timezone),
+      temperature: getTemperature(data.main.temp),
+      weather: data.weather[0].description,
+    };
+
+    printCurrentWeather(currentWeather);
+  } catch (err) {
+    // ska vi hantera detta med speciell text? ladda om sidan något gick fel
+    throw err;
+  }
+};
+fetchCurrentWeather(lat, long);
+//console.log(currentWeather);
+
+// fetch för forcast
+//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+// fetch direct geoCoding
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
