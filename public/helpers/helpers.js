7d9a30653affdf147c671b24f5b9d94e3c709266
Therese Linden <theresemlinden@gmail.com>
2023-02-17 17:15:19 +0100

    added logic to add a zero in front of hours and minutes so its displayed accordingly HH:MM

--- public/helpers/helpers.js
+++ public/helpers/helpers.js
@@ -1,7 +1,9 @@
 export const getLocalTime = (time, timezone = 0) => {
     const localTime = new Date((time + timezone) * 1000);
-    const getHours = localTime.getUTCHours();
-    const getMinutes = localTime.getUTCMinutes();
+    let getHours = localTime.getUTCHours();
+    let getMinutes = localTime.getUTCMinutes();
+    getHours = getHours < 10 ? '0' + getHours : getHours;
+    getMinutes = getMinutes < 10 ? '0' + getMinutes : getMinutes;
     return `${getHours}:${getMinutes}`;
 };
 export const getTemperature = (temp) => Math.round(temp);
